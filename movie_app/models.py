a296c5b58c6683352aa95681a26e1f9430844977
Usmanov Kanat <usmnvk200207@gmail.com>
2022-06-17 16:32:40 +0600

    Hw1

---
+++ movie_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    duration = models.TimeField()
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    text = models.TextField(max_length=250)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+# Create your models here.
